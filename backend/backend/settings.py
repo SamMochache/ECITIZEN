6c89b0ea44a25662ce8dc07e9489af4dd14f15be
SamMochache <sammochache01@gmail.com>
2025-05-26 22:57:25 +0300

    Remove frontend React application files and dependencies; add django-cors-headers to requirements

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,6 +62,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    config('CORS_ALLOWED_ORIGINS')
 ]
 
 ROOT_URLCONF = 'backend.urls'
